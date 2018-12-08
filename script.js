eab9177a43303396b28fd0d8ebdba418b252e1f5
YuliPolishchuk <44016256+YuliPolishchuk@users.noreply.github.com>
2018-12-08 14:11:56 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+var courseName = 'mateAcademy';
+var groupNames = ['fe_sep18', 'fe_oct18', 'fe_now18'];
+var nameOurGroup = "fe_oct18";
+var nameFeOct18Student = ['Sergey', 'Anton', 'Dima'];
+/*
+//alert
+var menu = [
+	[Number.NEGATIVE_INFINITY, 20, null, "", "Пробачте, в нашому закладі немає страв для вас :("],
+  [20, 50, "Ви можете купити колу. Бажаєте?", "Ви замовили колу", "Ви нічого не замовили"],
+  [50, 100, "Ви можете купити Чізбургер і картоплю. Бажаєте?", "Ви замовили Чізбургер і картоплю", "Ви нічого не замовили"],
+  [100, Number.POSITIVE_INFINITY, "Ви можете купити БігМакМеню. Бажаєте?", "Ви замовили БігМакМеню", "Ви нічого не замовили"],
+];
+var money = parseInt(prompt('Скільки грошей у вас є?', "0"));
+var menuItem = menu.find((menuItem) => money >= menuItem[0] && money < menuItem[1]);
+var answer = menuItem[2] ? confirm(menuItem[2]) : false;
+alert(menuItem[answer?3:4]);
+
+//console
+var menu = [
+	[Number.NEGATIVE_INFINITY, 20, null, "", "Пробачте, в нашому закладі немає страв для вас :("],
+  [20, 50, "Ви можете купити колу. Бажаєте?", "Ви замовили колу", "Ви нічого не замовили"],
+  [50, 100, "Ви можете купити Чізбургер і картоплю. Бажаєте?", "Ви замовили Чізбургер і картоплю", "Ви нічого не замовили"],
+  [100, Number.POSITIVE_INFINITY, "Ви можете купити БігМакМеню. Бажаєте?", "Ви замовили БігМакМеню", "Ви нічого не замовили"],
+];
+var money = parseInt(prompt('Скільки грошей у вас є?', "0"));
+var menuItem = menu.find((menuItem) => money >= menuItem[0] && money < menuItem[1]);
+var answer = menuItem[2] ? confirm(menuItem[2]) : false;
+console.log(menuItem[answer?3:4]);
+*/
+// Маша і Новий рік
+/*
+var questuionAngela = confirm ('Чи прийшла Анжела?', ''); 
+var questuionStepan = confirm ('Чи прийшов Степан?', ''); 
+var questuionMukola = confirm ('Чи прийшов Микола?', ''); 
+
+if (questuionAngela == false && questuionStepan == true && questuionMukola == true ) {
+  console.log('Маша була рада');
+} else if (questuionAngela == true && questuionStepan == false && questuionMukola == false ) {
+  console.log('Маша була рада');
+} else if (questuionAngela == true && questuionStepan == false && questuionMukola == true ) {
+  console.log('Маша була рада');
+} else if(questuionAngela == true && questuionStepan == true && questuionMukola == false ) {
+  console.log('Маша була рада');
+} else {
+  console.log('Маша була засмучена');
+}
+*/
+var timeStartSale = parseInt(prompt('Введіть час початку знижки', '0'));
+var timeEndSale = parseInt(prompt('Введіть час кінця знижки', '24'));
+var timeNow = parseInt(prompt('Введіть теперішній час', '0'));
+var active = (timeNow >= timeStartSale && timeNow <= timeEndSale) || (timeEndSale<timeStartSale && !(timeNow > timeEndSale && timeNow < timeStartSale));
+console.log(active ? 'Знижка діє' : 'Знижка не діє');
